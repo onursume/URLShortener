c4fe94ab51abd26a5211631a3f4192ca2acd4291
onursume <onursume05@gmail.com>
2020-05-30 23:08:54 +0300

    Reduced sleeping time that is needed to initialize couchbase

--- docker/couchbase/configure-node.sh
+++ docker/couchbase/configure-node.sh
@@ -5,7 +5,7 @@
  
  /entrypoint.sh couchbase-server &
  
- sleep 20
+ sleep 15
  
  # Setup index and memory quota
  curl -v POST http://127.0.0.1:8091/pools/default -d memoryQuota=2048 -d indexMemoryQuota=2048
@@ -22,6 +22,4 @@
  # Load bucket
  curl -v -u Administrator:password POST http://127.0.0.1:8091/pools/default/buckets -d name=Url -d authType=sasl -d saslPassword=123qwe -d bucketType=couchbase -d flushEnabled=1 -d ramQuotaMB=100
  
- sleep 5
-  
  fg 1
